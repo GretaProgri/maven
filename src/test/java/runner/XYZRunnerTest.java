a5f48ecac7e9177fbaa551223da9d2a74b0d6bcf
gretaprogri <gretaprogri@gretas-mbp-2.lan>
2022-05-19 22:16:46 -0400

    second commit

--- src/test/java/runner/XYZRunnerTest.java
+++ src/test/java/runner/XYZRunnerTest.java
@@ -1,16 +1,20 @@
 package runner;
 
+import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+@RunWith(Cucumber.class)
+@CucumberOptions (
+		
+		features="src/test/resources/orangehrm",
+		glue= {"steps"},
+		plugin= {"pretty", "html:target/cucumnber-report.html"},
+		monochrome = true
+		)
 
 public class XYZRunnerTest {
-	@RunWIht(Cucumber.class)
-	@CucumnberOptions (
-			
-			features="src/test/resources/orangehrm",
-			glue= {"steps"},
-			plugin= {"pretty", "html:target/cucumnber-report.html"},
-			monochrome = true
-			)
+
 		}
 
 
